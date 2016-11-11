c1570dcf2c51c97524c6813296ae018f2c961560
tyx222 <464984325@qq.com>
2016-11-11 11:03:52 +0800

    Add files via upload

---
+++ Date.js
@@ -0,0 +1,31 @@
+/*var age = 18;
+if (age > 18) {
+    console.log('成年了');
+}
+else {
+    console.log('未成年');
+}*/
+
+
+var person = ['小明', '小胖'];
+
+/*for (var i = 0; i < person.length; i++);
+{
+    console.log(i);
+    console.log(person[i]);
+
+}*/
+
+
+/*var i = 0;
+while (i < person.length) {
+    sonsole.log(person[i]);
+    i++;
+}*/
+
+var i = 0;
+do {
+    console.log(person[i]);
+    i++;
+}
+while (i < person.length)
